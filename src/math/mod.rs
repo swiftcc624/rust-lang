fa548f4c07398697a2dc45c5169e5d66d729a3be
swiftcc624 <swiftcc624@gmail.com>
2021-12-18 20:49:26 +0800

    first commit

---
+++ src/math/mod.rs
@@ -0,0 +1,17 @@
+mod extended_euclidean_algorithm;
+mod greatest_common_divisor;
+mod pascal_triangle;
+mod perfect_numbers;
+mod prime_check;
+mod prime_numbers;
+mod trial_division;
+
+pub use self::extended_euclidean_algorithm::extended_euclidean_algorithm;
+pub use self::greatest_common_divisor::{
+    greatest_common_divisor_iterative, greatest_common_divisor_recursive,
+};
+pub use self::pascal_triangle::pascal_triangle;
+pub use self::perfect_numbers::perfect_numbers;
+pub use self::prime_check::prime_check;
+pub use self::prime_numbers::prime_numbers;
+pub use self::trial_division::trial_division;
